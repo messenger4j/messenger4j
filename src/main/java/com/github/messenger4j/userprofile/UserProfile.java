afa0c8822750a433116cd596bef1df95997450a5
Raf Theunis <raf.theunis@kbc.be>
2018-09-12 13:15:33 +0200

    only use fields that don't require user's permission

--- src/main/java/com/github/messenger4j/userprofile/UserProfile.java
+++ src/main/java/com/github/messenger4j/userprofile/UserProfile.java
@@ -17,24 +17,12 @@ public final class UserProfile {
     private final String firstName;
     private final String lastName;
     private final String profilePicture;
-    private final String locale;
-    private final float timezoneOffset;
-    private final Gender gender;
-    private final boolean isPaymentEnabled;
-    private final Optional<Referral> lastAdReferral;
 
-    public UserProfile(@NonNull String firstName, @NonNull String lastName, @NonNull String profilePicture,
-                       @NonNull String locale, float timezoneOffset, @NonNull Gender gender, boolean isPaymentEnabled,
-                       @NonNull Optional<Referral> lastAdReferral) {
+    public UserProfile(@NonNull String firstName, @NonNull String lastName, @NonNull String profilePicture) {
 
         this.firstName = firstName;
         this.lastName = lastName;
         this.profilePicture = profilePicture;
-        this.locale = locale;
-        this.timezoneOffset = timezoneOffset;
-        this.gender = gender;
-        this.isPaymentEnabled = isPaymentEnabled;
-        this.lastAdReferral = lastAdReferral;
     }
 
     public String firstName() {
@@ -49,30 +37,4 @@ public final class UserProfile {
         return profilePicture;
     }
 
-    public String locale() {
-        return locale;
-    }
-
-    public float timezoneOffset() {
-        return timezoneOffset;
-    }
-
-    public Gender gender() {
-        return gender;
-    }
-
-    public boolean isPaymentEnabled() {
-        return isPaymentEnabled;
-    }
-
-    public Optional<Referral> lastAdReferral() {
-        return lastAdReferral;
-    }
-
-    /**
-     * @since 1.0.0
-     */
-    public enum Gender {
-        MALE, FEMALE
-    }
 }
